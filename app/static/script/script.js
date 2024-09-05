984a564952577364fb0f9c772632870a1717ed21
Aaron Labini <aaronbarlas.labini@gmail.com>
2024-09-05 22:02:11 +0800

    User profile

--- app/static/script/script.js
+++ app/static/script/script.js
@@ -1,3 +1,7 @@
 window.addEventListener("load", function (event) {
   document.querySelector('[data-dropdown-toggle="dropdown"]').click();
 });
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  document.getElementById("readProductButton").click();
+});
--- app/user.py
+++ app/user.py
@@ -2,6 +2,7 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
 from werkzeug.exceptions import abort
+from app.db import get_db
 import requests
 
 bp = Blueprint('user', __name__, url_prefix='/user')
@@ -16,6 +17,7 @@ def get_places_data(city, categories):
         "Authorization": FOURSQUARE_API_KEY
     }
     params = {}
+    city = city + ', Ph'
     
     if city:
         params["near"] = city
@@ -32,7 +34,7 @@ def get_places_data(city, categories):
                 fsq_id = place.get('fsq_id')
                 photo_response = requests.get(FOURSQUARE_API_PHOTOS_URL.format(fsq_id=fsq_id) + "?limit=1", headers=headers)
                 if photo_response.status_code == 200:
-                    photos = photo_response.json()  # This will return a list
+                    photos = photo_response.json()  # return a list
                     place['photos'] = photos if photos else []  # Add the list of photos if it exists
                 else:
                     place['photos'] = []
@@ -45,26 +47,22 @@ def index():
     from app.auth import login_required
     @login_required
     def inner():
-        
         if request.method == 'POST':
-            city = request.form.get('city')
-            if not city:
-                city = 'Philippines'
-            categories = request.form.get('categories')
-            city = city + ', Ph'
+            city = request.form.get('city', 'Philippines')
+            categories = request.form.get('category')
             places = get_places_data(city, categories)
         
             if places:
-                return render_template('user/index.html', places=places, city=city)
+                return render_template('user/index.html', places=places, city=city, categories=categories)
             else:
                 return 'Error: Failed to fetch places data.'
             return render_template('user/index.html')
         else:
             city = 'Philippines'
-            categories = '10000,12000,13000,19000,16000,17000'
+            categories = '12000,19000,16000,17000,10000,13000'
             places = get_places_data(city, categories)
             if places:
-                return render_template('user/index.html', places=places, city=city)
+                return render_template('user/index.html', places=places, city=city, categories=categories)
             else:
                 return 'Error: Failed to fetch places data.'
         return render_template('user/index.html')
@@ -75,6 +73,58 @@ def profile():
     from app.auth import login_required
     @login_required
     def inner():
+        if request.method == 'POST':
+            firstname = request.form['firstname']
+            lastname = request.form['lastname']
+            email = request.form['email']
+            error = None
+            
+            if not firstname:
+                error = 'First name is required.'
+            elif not lastname:
+                error = 'Last name is required.'
+            elif not email:
+                error = 'Email is required.'
+           
+            if error is None:
+                db = get_db()
+                db.execute(
+                    "UPDATE user SET firstname = ?, lastname = ?, email = ?"
+                    " WHERE id = ?",
+                    (firstname, lastname, email, g.user['id']),
+                )
+                db.commit()
+                return redirect(url_for("user.profile"))
+            
+            flash(error)
+            return render_template('user/profile.html', user=g.user)
         return render_template('user/profile.html')
     return inner()
+
+@bp.route('/update/character', methods=('GET', 'POST'))
+def update_character():
+    from app.auth import login_required
+    @login_required
+    def inner():
+        if request.method == 'POST':
+            character = request.form['character']
+            error = None
+            if not character:
+                error = 'You need to choose character'
+           
+            if error is None:
+                db = get_db()
+                db.execute(
+                    "UPDATE user SET character = ?"
+                    " WHERE id = ?",
+                    (character, g.user['id']),
+                )
+                db.commit()
+                return redirect(url_for("user.profile"))
+            
+            flash(error)
+            return render_template('user/profile.html', user=g.user)
+        return render_template('user/profile.html')
+    return inner()
+
 
